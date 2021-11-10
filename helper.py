7ed2557c3f4b62e1edb1f0c60c04061ef69ac319
蔡維哲 <108356037@nccu.edu.tw>
2021-11-10 17:01:25 +0800

    filter out .DS_Store

--- helper.py
+++ helper.py
@@ -1,23 +1,29 @@
+#!/usr/bin/python3
 # -- coding:UTF-8 --
 import xlsxwriter
-
+import re
 import os
 from datetime import datetime
+import shutil
 
 image_dir = str(input("輸入照片所在的資料夾名稱："))
-image_dir = './' + image_dir
+image_dir = '/Users/henry/Desktop/' + image_dir
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
 xlxs_name = dt_string+'-牙齒筆記.xlsx'
 workbook = xlsxwriter.Workbook(xlxs_name)
 
-unsort_files = [x for x in os.listdir(image_dir)]
+pattern = re.compile("(截圖|20).*")
+unsort_files_with_ds = [x for x in os.listdir(image_dir)]
+unsort_files = list(filter(pattern.match, unsort_files_with_ds))
+print(unsort_files)
 for i, f in enumerate(unsort_files):
     unsort_files[i] = f.lstrip('截圖 ').replace(
-        '下午', 'PM-').replace('上午', 'AM-').rstrip('.png')
+        '下午', 'PM-').replace('上午', 'AM-').rstrip('.png').rstrip('拷貝')
     os.rename(image_dir+'/'+f, image_dir+'/' + unsort_files[i]+'.png')
 
-# print(unsort_files)
+
+
 sort_datetime = sorted([datetime.strptime(
     ts, "%Y-%m-%d %p-%I.%M.%S") for ts in unsort_files])
 # print(sort_datetime)
@@ -33,3 +39,5 @@ for i, img in enumerate(sort_png_time):
     worksheet.insert_image('A2', image_dir+'/'+img+'.png')
 
 workbook.close()
+
+shutil.move('/Users/henry/'+xlxs_name, '/Users/henry/Desktop/'+xlxs_name)
